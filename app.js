8bd5d375f4179c4db9e97a047ad0c87abc71bf0d
Hardik Kumar <hardikkumar32@gmail.com>
2022-03-18 18:30:50 +0530

    Displaying reviews Deleting reviews Deleting Middleware

--- app.js
+++ app.js
@@ -65,7 +65,9 @@ app.post(
 app.get(
   "/campgrounds/:id",
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate(
+      "reviews"
+    );
     res.render("campgrounds/show", { campground });
   })
 );
@@ -103,6 +105,15 @@ app.post(
     await review.save();
     await campground.save();
     res.redirect(`/campgrounds/${campground.id}`);
+  })
+);
+app.delete(
+  "/campgrounds/:id/reviews/:reviewId",
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
   })
 );
 app.all("*", (req, res, next) => {
