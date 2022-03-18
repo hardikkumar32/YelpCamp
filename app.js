5fec266c0d3e3dc36956d8ae1b74b49f2f74d96b
Hardik Kumar <hardikkumar32@gmail.com>
2022-03-18 16:31:48 +0530

    Reviews created and validated

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const app = express();
 const path = require("path");
 const ejsMate = require("ejs-mate");
 // const Joi = require("joi");
-const { campgroundSchema } = require("./schemas.js");
+const { campgroundSchema, reviewSchema } = require("./schemas.js");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 app.engine("ejs", ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 const Campground = require("./models/campground");
+const Review = require("./models/review");
+
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   //   useCreateIndex: true,
@@ -34,6 +36,13 @@ const validatecampground = (req, res, next) => {
   } else next();
 };
 
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, "400");
+  } else next();
+};
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -82,6 +91,18 @@ app.delete(
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
+  })
+);
+app.post(
+  "/campgrounds/:id/reviews",
+  validateReview,
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground.id}`);
   })
 );
 app.all("*", (req, res, next) => {
